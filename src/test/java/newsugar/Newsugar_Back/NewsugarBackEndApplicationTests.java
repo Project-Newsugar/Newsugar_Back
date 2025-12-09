2a8fe2c5f33aa6ddcd1e7a3a2814e480325148f0
wldsmdl7 <wldmsdl5835@gmail.com>
2025-12-09 10:25:05 +0900

    fix : develop-main  충돌 해결

--- src/test/java/newsugar/Newsugar_Back/NewsugarBackEndApplicationTests.java
+++ src/test/java/newsugar/Newsugar_Back/NewsugarBackEndApplicationTests.java
@@ -3,17 +3,11 @@ package newsugar.Newsugar_Back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
-@SpringBootTest
-class NewsugarBackEndApplicationTests {
-
-=======
 @SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 
 class NewsugarBackEndApplicationTests {
->>>>>>> origin/main
 	@Test
 	void contextLoads() {
 	}
