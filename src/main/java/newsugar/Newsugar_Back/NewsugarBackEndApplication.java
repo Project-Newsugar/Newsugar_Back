479ed038df523bb255ed7b40173695db5b74b6cd
KimHaeCho <jiyun5823@gmail.com>
2025-12-23 05:22:18 +0900

    Fix: Set default timezone to Asia/Seoul

--- src/main/java/newsugar/Newsugar_Back/NewsugarBackEndApplication.java
+++ src/main/java/newsugar/Newsugar_Back/NewsugarBackEndApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class NewsugarBackEndApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NewsugarBackEndApplication.class, args);
